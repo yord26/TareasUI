81b0ad35be75e7056915afba682d1d282ab47b71
Yordan <yordan.dev25@gmail.com>
2026-05-22 15:17:11 -0500

    Agrega componente Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+      <footer style={{ background: "#333", padding: "1rem", color: "white", textAlign: "center" }}>
+        <p>© 2026 TareasUI - Yordan</p>
+      </footer>
+    )
+  }
+  
+  export default Footer
