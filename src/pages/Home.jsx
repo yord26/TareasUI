8d346b7d3853459ee83204480c387d5c61cb2e02
Yordan <yordan.dev25@gmail.com>
2026-05-22 15:20:25 -0500

    Desarrolla página Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+      <div style={{ padding: "2rem" }}>
+        <h1>Bienvenido a TareasUI</h1>
+        <p>Gestiona tus tareas diarias de forma sencilla.</p>
+      </div>
+    )
+  }
+  
+  export default Home
