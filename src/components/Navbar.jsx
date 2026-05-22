c5e3a3e724267583d5e7124346836ae3b0d4f5a7
Yordan <yordan.dev25@gmail.com>
2026-05-22 15:17:03 -0500

    Agrega componente Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+    return (
+      <nav style={{ background: "#333", padding: "1rem", color: "white" }}>
+        <h2>TareasUI</h2>
+      </nav>
+    )
+  }
+  
+  export default Navbar
