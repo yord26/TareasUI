a4ca9dbeff5832815700aa9a2bf90ac7f2000729
Yordan <yordan.dev25@gmail.com>
2026-05-22 15:21:48 -0500

    Agrega página About

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+    return (
+      <div style={{ padding: "2rem" }}>
+        <h1>Acerca de TareasUI</h1>
+        <p>Aplicación desarrollada por Yordan como parte del Seminario de Habilidades Prácticas II.</p>
+      </div>
+    )
+  }
+  
+  export default About
