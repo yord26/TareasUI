8982a1bdde8662ebb633567a93b577d4e2fe012c
Jamer <juansqsw@gmail.com>
2026-05-22 16:28:34 -0500

    Integra Navbar y enrutamiento en App

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
+import { Link } from 'react-router-dom'
+
 function Navbar() {
     return (
-      <nav style={{ background: "#333", padding: "1rem", color: "white" }}>
-        <h2>TareasUI</h2>
+      <nav style={{ background: "#333", padding: "1rem", color: "white", display: "flex", gap: "1.5rem", alignItems: "center" }}>
+        <h2 style={{ margin: 0 }}>TareasUI</h2>
+        <Link to="/" style={{ color: "white", textDecoration: "none" }}>Inicio</Link>
+        <Link to="/formulario" style={{ color: "white", textDecoration: "none" }}>Formulario</Link>
+        <Link to="/about" style={{ color: "white", textDecoration: "none" }}>Acerca de</Link>
       </nav>
     )
   }
-  
+
   export default Navbar
